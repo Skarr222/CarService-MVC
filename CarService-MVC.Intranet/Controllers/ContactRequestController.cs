8b633db709964684b3db0e950680f5d89aff2812
Skarr222 <oskargomza54@gmail.com>
2026-05-03 22:32:48 +0200

    quick push

---
+++ CarService-MVC.Intranet/Controllers/ContactRequestController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarService_MVC.Intranet.Controllers;
+
+public class ContactRequestController : Controller
+{
+    public IActionResult Index() => View();
+
+    public IActionResult Edit(int id) => View();
+
+    [HttpPost]
+    public IActionResult Edit(object model) => RedirectToAction("Index");
+
+    public IActionResult Delete(int id) => View();
+
+    [HttpPost, ActionName("Delete")]
+    public IActionResult DeleteConfirmed(int id) => RedirectToAction("Index");
+}
